b4690e18c47990612df4e1ddc6fb53b3ec1fd769
bas.suckling <suckling.w.s@gmail.com>
2020-04-22 09:36:45 +1200

    v3 of application

--- version/app.js
+++ version/app.js
@@ -5,16 +5,16 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 2 of the app.' + '\n';
+    response = 'This is version 3 of the app.' + '\n';
 
     //send the response to the client
     res.send(response);
 
 });
 
-app.get('/test', function (req, res) {
+app.get('/test2', function (req, res) {
 
-  response = 'This is version 2 of the app on the test route.' + '\n';
+  response = 'This is version 3 of the app on the test2 route.' + '\n';
 
   //send the response to the client
   res.send(response);
